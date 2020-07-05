025866f34a24d5c944769170a5147e98edf2c630
Ilich <ilichh1@hotmail.com>
2020-07-05 14:20:44 -0500

    Model & migration for TODO object

---
+++ node/models/todo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+
+  Todo.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  }, {
+    sequelize,
+    paranoid: true,
+    modelName: 'Todo',
+  });
+
+  return Todo;
+};
