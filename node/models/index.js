7093822f0efff40f7a2590946904b62e6dd8c26e
Ilich <ilichh1@hotmail.com>
2020-07-05 14:20:18 -0500

    Default sequelize models import and setup

--- node/models/index.js
+++ node/models/index.js
@@ -5,9 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+// Determinar la configuración de config/config.JSON
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -15,6 +16,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// Importar los scripts que representan los modelos en la carpeta models/*.js
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -25,12 +27,14 @@ fs
     db[model.name] = model;
   });
 
+// Una vez importados los modelos, asocian al objeto "db" que se exportará en este archivo
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+// Se asocian los objetos de sequelize para esten disponibles y evitar más importaciones / exportaciones
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
